bc9d234d07ca8c659b5f1f64a4340a37a6319e85
Bajpan Gosh <bajpangosh@users.noreply.github.com>
2018-06-20 21:49:08 +0530

    Create sub.sh

---
+++ sub.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+# GET ALL USER INPUT
+echo "Domain Name (eg. example.com)?"
+read DOMAIN
+echo "Username (eg. mywordpress)?"
+read USERNAME
+
+echo "Updating OS................."
+sudo apt-get update
+sudo apt install pwgen -y
+
+echo "Sit back and relax :) ......"
+cd /etc/nginx/sites-available/
+sudo wget -O "$DOMAIN" https://goo.gl/T3YBrn
+sudo sed -i -e "s/example.com/$DOMAIN/" "$DOMAIN"
+sudo sed -i -e "s/www.example.com/www.$DOMAIN/" "$DOMAIN"
+sudo ln -s /etc/nginx/sites-available/"$DOMAIN" /etc/nginx/sites-enabled/
+echo "Setting up Wordpress With Cloudflare FULL SSL"
+sleep 2;
+sudo mkdir /var/www/"$DOMAIN"
+cd /var/www/"$DOMAIN"
+sudo wget -O robots.txt https://goo.gl/a6oPLq
+sudo su -c 'echo "<?php phpinfo(); ?>" |tee info.php'
+cd ~
+wget wordpress.org/latest.zip
+unzip latest.zip
+mv /root/wordpress/* /var/www/"$DOMAIN"/
+rm -rf wordpress latest.zip
+
+echo "Nginx server installation completed"
+sleep 2;
+cd ~
+sudo chown www-data:www-data -R /var/www/"$DOMAIN"
+sudo systemctl restart nginx.service
+
+PASS=`pwgen -s 14 1`
+
+mysql -uroot <<MYSQL_SCRIPT
+CREATE DATABASE $USERNAME;
+CREATE USER '$USERNAME'@'localhost' IDENTIFIED BY '$PASS';
+GRANT ALL PRIVILEGES ON $USERNAME.* TO '$USERNAME'@'localhost';
+FLUSH PRIVILEGES;
+MYSQL_SCRIPT
+
+echo "Here is the database"
+echo "Database:   $USERNAME"
+echo "Username:   $USERNAME"
+echo "Password:   $PASS"
