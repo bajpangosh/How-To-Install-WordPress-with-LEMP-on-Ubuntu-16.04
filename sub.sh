1471ea7a47e84f156ee2229f7bdcaf80b34f6936
Bajpan Gosh <bajpangosh@users.noreply.github.com>
2018-06-20 22:47:26 +0530

    Update sub.sh

--- sub.sh
+++ sub.sh
@@ -22,10 +22,10 @@ cd /var/www/"$DOMAIN"
 sudo wget -O robots.txt https://goo.gl/a6oPLq
 sudo su -c 'echo "<?php phpinfo(); ?>" |tee info.php'
 cd ~
-wget wordpress.org/latest.zip
-unzip latest.zip
-mv /root/wordpress/* /var/www/"$DOMAIN"/
-rm -rf wordpress latest.zip
+sudo  wget wordpress.org/latest.zip
+sudo  unzip latest.zip
+sudo  mv wordpress/* /var/www/"$DOMAIN"/
+sudo rm -rf wordpress latest.zip
 
 echo "Nginx server installation completed"
 sleep 2;
@@ -35,7 +35,7 @@ sudo systemctl restart nginx.service
 
 PASS=`pwgen -s 14 1`
 
-mysql -uroot <<MYSQL_SCRIPT
+sudo mysql -uroot <<MYSQL_SCRIPT
 CREATE DATABASE $USERNAME;
 CREATE USER '$USERNAME'@'localhost' IDENTIFIED BY '$PASS';
 GRANT ALL PRIVILEGES ON $USERNAME.* TO '$USERNAME'@'localhost';
