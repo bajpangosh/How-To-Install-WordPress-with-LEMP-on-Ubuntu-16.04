0e03e58de245db199b3083705f2c686f9309e089
Bajpan Gosh <bajpangosh@users.noreply.github.com>
2018-10-09 12:15:20 +0530

    Update wpx-install.sh

--- wpx-install.sh
+++ wpx-install.sh
@@ -25,9 +25,11 @@ sudo ln -s /etc/nginx/sites-available/"$DOMAIN" /etc/nginx/sites-enabled/
 
 echo "Setting up Cloudflare FULL SSL"
 sleep 2;
-sudo mkdir /etc/nginx/ssl
-sudo openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/nginx/ssl/nginx.key -out /etc/nginx/ssl/nginx.crt
-sudo openssl dhparam -out /etc/nginx/ssl/dhparam.pem 2048
+sudo add-apt-repository ppa:certbot/certbot
+sudo apt install python-certbot-nginx
+sudo certbot --nginx -d $DOMAIN -d www.$DOMAIN
+sudo certbot renew --dry-run
+
 cd /etc/nginx/
 sudo mv nginx.conf nginx.conf.backup
 sudo wget -O nginx.conf https://goo.gl/n8crcR
